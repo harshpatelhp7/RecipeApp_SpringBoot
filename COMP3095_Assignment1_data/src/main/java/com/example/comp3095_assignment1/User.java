ed1b6d65536960d197a4b476bb96653686efb29c
Ming Yang <ming.yang@georgebrown.ca>
2021-11-06 02:15:24 -0400

    Updated Register and Log in

---
+++ COMP3095_Assignment1_data/src/main/java/com/example/comp3095_assignment1/User.java
@@ -0,0 +1,54 @@
+package com.example.comp3095_assignment1;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    private int id;
+    private String username;
+    private String password;
+    private String firstname;
+    private String lastname;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
