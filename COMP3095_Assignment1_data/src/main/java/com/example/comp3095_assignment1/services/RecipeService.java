69b33dd00bbfcb0e64746dcded72bb2ef2421f9c
Harsh <harshpatelhp728@gmail.com>
2021-11-07 22:12:27 -0500

    adding recipe services

--- COMP3095_Assignment1_data/src/main/java/com/example/comp3095_assignment1/services/RecipeService.java
+++ COMP3095_Assignment1_data/src/main/java/com/example/comp3095_assignment1/services/RecipeService.java
@@ -1,7 +1,23 @@
 package com.example.comp3095_assignment1.services;
 
+import com.example.comp3095_assignment1.model.Recipe;
+import com.example.comp3095_assignment1.repositories.RecipeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public interface RecipeService {
+public class RecipeService {
+
+    @Autowired
+    private RecipeRepository recipeRepository;
+
+    public Recipe searchRecipe(String name) {
+            return recipeRepository.findByName(name);
+    }
+
+    public List<Recipe> viewRecipe(){
+        return recipeRepository.findAll();
+    }
 }
